2aaac581d7e8062caca6d413e44538076d903824
JuanLeycal <leyva.calle1403@gmail.com>
2021-04-19 00:05:22 -0500

    GeoQuiz terminado con el reto
    Cambie el color de la aplicación y arreglé el index out of range de las preguntas.

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,10 +2,78 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var questions: ArrayList<Question>
+    var position = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadQuestion()
+        setupViews()
+    }
+
+    private fun loadQuestion() {
+
+        questions = ArrayList()
+
+        var question = Question("¿Es Perú sólo Lima?", true)
+        questions.add(question)
+
+        question = Question("¿Es el Huáscar una maravilla del Mundo?", false)
+        questions.add(question)
+
+        question = Question("¿Es Venezuela el país donde esta Caracas?", true)
+        questions.add(question)
+
+        question = Question("La capital de Perú es Cuzco?", false)
+        questions.add(question)
+    }
+
+    private fun setupViews() {
+
+        textView.text = questions[position].sentence
+        btYes.setOnClickListener(){
+            if(questions[position].answer) {
+                Toast.makeText(this, "Correcto", Toast.LENGTH_LONG).show()
+                position++
+                if (position == 4) {
+                    position = 0
+                }
+                textView.text = questions[position].sentence
+            }
+            else{
+                Toast.makeText(this, "Incorrecto", Toast.LENGTH_LONG).show()
+            }
+
+    }
+        btNo.setOnClickListener(){
+            if(!questions[position].answer) {
+                Toast.makeText(this, "Correcto", Toast.LENGTH_LONG).show()
+                position++
+                if (position == 4) {
+                    position = 0
+                }
+                textView.text = questions[position].sentence
+            }
+            else{
+                Toast.makeText(this, "Incorrecto", Toast.LENGTH_LONG).show()
+            }
+
+        }
+        btNext.setOnClickListener{
+            position++
+            if(position == 4){
+                position = 0
+            }
+            textView.text = questions[position].sentence
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/geoquiz/Question.kt
@@ -0,0 +1,7 @@
+package com.example.geoquiz
+
+class Question(
+    val sentence: String,
+    val answer: Boolean
+    )
+
